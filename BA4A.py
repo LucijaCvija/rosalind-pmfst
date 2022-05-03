fe4deaaf92e5aa372efb56b9e435fd553a37d77c
LucijaCvija <101116346+LucijaCvija@users.noreply.github.com>
2022-05-03 14:54:28 +0200

    Create BA4A.py

---
+++ BA4A.py
@@ -0,0 +1,82 @@
+def get_genetic_code_codons():
+    genetic_code = {
+        "AAA": "K",
+        "AAC": "N",
+        "AAG": "K",
+        "AAU": "N",
+        "ACA": "T",
+        "ACC": "T",
+        "ACG": "T",
+        "ACU": "T",
+        "AGA": "R",
+        "AGC": "S",
+        "AGG": "R",
+        "AGU": "S",
+        "AUA": "I",
+        "AUC": "I",
+        "AUG": "M",
+        "AUU": "I",
+        "CAA": "Q",
+        "CAC": "H",
+        "CAG": "Q",
+        "CAU": "H",
+        "CCA": "P",
+        "CCC": "P",
+        "CCG": "P",
+        "CCU": "P",
+        "CGA": "R",
+        "CGC": "R",
+        "CGG": "R",
+        "CGU": "R",
+        "CUA": "L",
+        "CUC": "L",
+        "CUG": "L",
+        "CUU": "L",
+        "GAA": "E",
+        "GAC": "D",
+        "GAG": "E",
+        "GAU": "D",
+        "GCA": "A",
+        "GCC": "A",
+        "GCG": "A",
+        "GCU": "A",
+        "GGA": "G",
+        "GGC": "G",
+        "GGG": "G",
+        "GGU": "G",
+        "GUA": "V",
+        "GUC": "V",
+        "GUG": "V",
+        "GUU": "V",
+        "UAA": "*",
+        "UAC": "Y",
+        "UAG": "*",
+        "UAU": "Y",
+        "UCA": "S",
+        "UCC": "S",
+        "UCG": "S",
+        "UCU": "S",
+        "UGA": "*",
+        "UGC": "C",
+        "UGG": "W",
+        "UGU": "C",
+        "UUA": "L",
+        "UUC": "F",
+        "UUG": "L",
+        "UUU": "F",
+    }
+    return genetic_code
+  
+  def Translation(rna_pattern):
+  G=get_genetic_code_codons()
+  rezultat=[]
+  for i in range(0, len(rna_pattern), 3):
+    slovo = G[rna_pattern[i : i + 3]]
+    print(slovo)
+    if(slovo == "*"):
+        break
+    rezultat.append(slovo)
+  rezultat = "".join(rezultat)
+  return rezultat
+
+Translation(rna_pattern)
